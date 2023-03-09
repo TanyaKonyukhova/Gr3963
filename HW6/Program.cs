d64cbe376511c00b5c42958c2147eee7d622eafb
Tatiana Konuykhova <lady.dzhiya@mail.ru>
2023-03-09 17:57:40 +0500

    add solution for hw6

---
+++ HW6/Program.cs
@@ -0,0 +1,37 @@
+﻿//41
+
+//ввод кол-ва повторений  
+Console.WriteLine("Сколько чисел Вы хотите ввести? ");
+int quantity = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+
+//цикл на ввод и проверку
+for(int i = 0; i < quantity; i++){
+
+    Console.WriteLine("Введите число ");
+    int number = Convert.ToInt32(Console.ReadLine());
+
+    if (number > 0){
+        count++;
+    }
+}
+
+//результат
+Console.WriteLine("Всего " + count + " положительных чисел");
+
+//43
+
+//ввод переменных
+Console.WriteLine("Введите значение b1 ");
+int b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение k1 ");
+int k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение b2 ");
+int b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение k2 ");
+int k2 = Convert.ToInt32(Console.ReadLine());
+
+double x = -(b1 - b2) / (k1 - k2);
+double y = k1 * x + b1;
+
+Console.Write($"Точка пересечения: [{x},{y}]");
